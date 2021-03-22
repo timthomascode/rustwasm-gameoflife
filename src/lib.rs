abc4568caafde305042a933a4ffac73a73635df9
timthomasdev <tim.thomas@hey.com>
2021-03-22 14:17:51 -0500

    Remove fps counter, timers

--- src/lib.rs
+++ src/lib.rs
@@ -5,24 +5,6 @@ mod utils;
 
 use std::fmt;
 use wasm_bindgen::prelude::*;
-use web_sys::console;
-
-pub struct Timer<'a> {
-    name: &'a str,
-}
-
-impl<'a> Timer<'a> {
-    pub fn new(name: &'a str) -> Timer<'a> {
-        console::time_with_label(name);
-        Timer { name }
-    }
-}
-
-impl<'a> Drop for Timer<'a> {
-    fn drop(&mut self) {
-        console::time_end_with_label(self.name);
-    }
-}
 
 #[wasm_bindgen]
 #[repr(u8)]
